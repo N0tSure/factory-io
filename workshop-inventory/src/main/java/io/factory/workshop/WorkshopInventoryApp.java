f5629e57945565ddc2c4ae85c870c846e122de14
Artem Sirosh <ASir2089@gmail.com>
2022-06-29 00:31:54 +0300

    PE0059 Enable EditorConfig integration
     * Added .editorconfig file
     * Fixed typo

--- workshop-inventory/src/main/java/io/factory/workshop/WorkshopInventoryApp.java
+++ workshop-inventory/src/main/java/io/factory/workshop/WorkshopInventoryApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopInventoryApp {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopManagementApp.class, args);
+		SpringApplication.run(WorkshopInventoryApp.class, args);
 	}
 }
